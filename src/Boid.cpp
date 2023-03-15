a5dab5decd69cfec1ef5fa8b210a7f736199092c
Greg0s <gregoire.tinnes@outlook.fr>
2023-03-15 15:43:37 +0100

    Add basic setters and getters
    Co-authored-by: emmaveauvy <emmaveauvy@users.noreply.github.com>

--- src/Boid.cpp
+++ src/Boid.cpp
@@ -1,8 +1,24 @@
 #include "./Boid.hpp"
 
-glm::vec2 Boid::getPosition()
+glm::vec2 Boid::getPosition() const
 {
     return pos;
+}
+
+glm::vec2 Boid::getDirection() const
+{
+    return dir;
+}
+
+glm::vec2 Boid::getSpeed() const
+{
+    return v;
+}
+
+void Boid::showBoid()
+{
+    std::cout << "-------BOID----------" << std::endl;
+    std::cout << "Pos: x " << pos.x << " y " << pos.y;
 }
 
 float Boid::getX() const
@@ -14,75 +30,91 @@ float Boid::getY() const
     return pos.y;
 }
 
-void Boid::separation(std::vector<Boid> boids, uint boidNum)
+void Boid::setPosition(const glm::vec2& newPos)
 {
-    Boid        boid        = boids.at(boidNum);
-    const float avoidFactor = 0.05;
-
-    glm::vec2 dir = {0.f, 0.f};
-    for (uint i = 0; i < boids.size(); i++)
-    {
-        if (i != boidNum)
-        {
-            if (isNear(boid))
-            {
-                dir.x += boid.pos.x - boids.at(i).getX();
-                dir.y += boid.pos.y - boids.at(i).getY();
-            }
-        }
-    }
-
-    boid.dir.x = dir.x * avoidFactor;
-    boid.dir.y = dir.y * avoidFactor;
+    pos.x = newPos.x;
+    pos.y = newPos.y;
 }
 
-bool Boid::isNear(Boid boid)
+void Boid::setDirectionX(const float& dirX)
 {
-    const float minDistance = 0.1;
-
-    float currentX = boid.getX();
-    float currentY = boid.getY();
-    float boidX    = this->getX();
-    float boidY    = this->getY();
-
-    return currentX < (boidX + minDistance) || currentX > (boidX - minDistance) || currentY > (boidY - minDistance) || currentY < (boidY + minDistance);
+    dir.x = dirX;
 }
 
-// glm::vec2 Boid::separationForce(std::vector<Boid> boids, uint boid)
+void Boid::setDirectionY(const float& dirY)
+{
+    dir.y = dirY;
+}
+
+// void Boid::separation(std::vector<Boid> boids, uint boidNum)
 // {
-//     glm::vec2 totalForce = {0, 0};
+//     Boid        boid        = boids.at(boidNum);
+//     const float avoidFactor = 0.05;
 
-//     glm::vec2 boidPos = boids.at(boid).getPosition();
-
-//     std::vector<Boid> nearBoids = getNearBoids(boids, boid);
-//     for (auto nearBoid : nearBoids)
-//     {
-//         totalForce.x += boidPos.x - nearBoid.getPosition().x;
-//         totalForce.y += boidPos.y - nearBoid.getPosition().y;
-//     }
-//     return totalForce;
-// }
-
-// std::vector<Boid> Boid::getNearBoids(std::vector<Boid> boids, uint boid)
-// {
-//     std::vector<Boid> nearBoids;
-
-//     const float minDistance = 0.1;
-
-//     float boidX = boids.at(boid).getPosition().x;
-//     float boidY = boids.at(boid).getPosition().y;
+//     glm::vec2 dir = {0.f, 0.f};
 //     for (uint i = 0; i < boids.size(); i++)
 //     {
-//         float currentX = boids.at(i).getPosition().x;
-//         float currentY = boids.at(i).getPosition().y;
-
-//         if (currentX < (boidX + minDistance) || currentX > (boidX - minDistance) || currentY > (boidY - minDistance) || currentY < (boidY + minDistance))
+//         if (i != boidNum)
 //         {
-//             nearBoids.push_back(boids.at(i));
+//             if (isNear(boid))
+//             {
+//                 dir.x += boid.pos.x - boids.at(i).getX();
+//                 dir.y += boid.pos.y - boids.at(i).getY();
+//             }
 //         }
 //     }
-//     return nearBoids;
+
+//     boid.dir.x = dir.x * avoidFactor;
+//     boid.dir.y = dir.y * avoidFactor;
 // }
+
+// bool Boid::isNear(Boid boid)
+// {
+//     const float minDistance = 0.1;
+
+//     float currentX = boid.getX();
+//     float currentY = boid.getY();
+//     float boidX    = this->getX();
+//     float boidY    = this->getY();
+
+//     return currentX < (boidX + minDistance) || currentX > (boidX - minDistance) || currentY > (boidY - minDistance) || currentY < (boidY + minDistance);
+// }
+
+glm::vec2 Boid::separationForce(std::vector<Boid> boids, uint boid)
+{
+    glm::vec2 totalForce = {0, 0};
+
+    glm::vec2 boidPos = boids.at(boid).getPosition();
+
+    std::vector<Boid> nearBoids = getNearBoids(boids, boid);
+    for (auto nearBoid : nearBoids)
+    {
+        totalForce.x += boidPos.x - nearBoid.getPosition().x;
+        totalForce.y += boidPos.y - nearBoid.getPosition().y;
+    }
+    return totalForce;
+}
+
+std::vector<Boid> Boid::getNearBoids(std::vector<Boid> boids, uint boid)
+{
+    std::vector<Boid> nearBoids;
+
+    const float minDistance = 0.1;
+
+    float boidX = boids.at(boid).getPosition().x;
+    float boidY = boids.at(boid).getPosition().y;
+    for (uint i = 0; i < boids.size(); i++)
+    {
+        float currentX = boids.at(i).getPosition().x;
+        float currentY = boids.at(i).getPosition().y;
+
+        if (currentX < (boidX + minDistance) || currentX > (boidX - minDistance) || currentY > (boidY - minDistance) || currentY < (boidY + minDistance))
+        {
+            nearBoids.push_back(boids.at(i));
+        }
+    }
+    return nearBoids;
+}
 
 // glm::vec2 getCenter(std::vector<Boid::Boid> boids)
 // {
--- src/Boid.hpp
+++ src/Boid.hpp
@@ -4,16 +4,25 @@ class Boid {
 private:
     glm::vec2 pos;
     glm::vec2 dir;
-    float     v;
+    glm::vec2 v;
     float     a;
 
 public:
-    explicit Boid(glm::vec2 position = {0, 0}, float vitesse = 1, glm::vec2 direction = {0, 0}, float acceleration = 0)
-        : pos(position), dir(direction), v(vitesse), a(acceleration){};
+    explicit Boid(glm::vec2 position = {0, 0}, glm::vec2 direction = {0, 0}, glm::vec2 speed = {1, 1}, float acceleration = 0)
+        : pos(position), dir(direction), v(speed), a(acceleration){};
 
-    glm::vec2         getPosition();
-    float             getX() const;
-    float             getY() const;
+    glm::vec2 getPosition() const;
+    glm::vec2 getDirection() const;
+    glm::vec2 getSpeed() const;
+    float     getX() const;
+    float     getY() const;
+
+    void setPosition(const glm::vec2& newPos);
+    void setDirectionX(const float& dirX);
+    void setDirectionY(const float& dirY);
+
+    void showBoid();
+
     glm::vec2         separationForce(std::vector<Boid> boids, uint boid);
     std::vector<Boid> getNearBoids(std::vector<Boid> boids, uint boid);
     void              separation(std::vector<Boid> boids, uint boidNum);
