5064c17cbd16fb8b3f77a7d8c655736ee25aacc6
Greg0s <gregoire.tinnes@outlook.fr>
2023-05-02 10:57:34 +0200

    move setters to private

--- src/Boid.hpp
+++ src/Boid.hpp
@@ -7,6 +7,10 @@ private:
     glm::vec2 v;
     float     a;
 
+    void setPosition(const glm::vec2& newPos);
+    void setDirectionX(const float& dirX);
+    void setDirectionY(const float& dirY);
+
 public:
     explicit Boid(glm::vec2 position = {0, 0}, glm::vec2 direction = {0, 0}, glm::vec2 speed = {1, 1}, float acceleration = 0)
         : pos(position), dir(direction), v(speed), a(acceleration){};
@@ -17,18 +21,12 @@ public:
     float     getX() const;
     float     getY() const;
 
-    void setPosition(const glm::vec2& newPos);
-    void setDirectionX(const float& dirX);
-    void setDirectionY(const float& dirY);
-
     void printBoid() const;
 
     void move();
     void inSquare(const glm::vec2& squareSize, const float& size, const float& strength, const float& scope);
 
-    void              separationForce(const std::vector<Boid>& boids, float scope, float strength);
-    std::vector<Boid> getNearBoids(const std::vector<Boid>& boids, float scope);
-
+    void separationForce(const std::vector<Boid>& boids, float scope, float strength);
     void alignementForce(const std::vector<Boid>& boids, const float& scope, const float& strength);
     void cohesionForce(const std::vector<Boid>& boids, const float& scope, const float& strength);
 };
